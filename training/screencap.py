513768785964c812e704a36d119a667396b27756
Rustam <dknext99@gmail.com>
2018-03-04 23:57:20 +0900

    Requirements added

--- training/screencap.py
+++ training/screencap.py
@@ -1,52 +1,54 @@
+# Done by Frannecklp
+
 import cv2
 import numpy as np
 import win32gui, win32ui, win32con
 
 # name of the window to grab
+# winName = "Grand Theft Auto V"
 winName = "Untitled - Notepad"
 
 
-def grab_screen(region=None):
+def grab_screen():
     desktop = win32gui.GetDesktopWindow()
 
-    while True:
-        # screen capture region
-        if region:
-            left, top, x2, y2 = region
-            width = x2 - left + 1
-            height = y2 - top + 1
-        else:
-            gtawin = win32gui.FindWindow(None, winName)
-            # get the bounding box of the window
-            left, top, x2, y2 = win32gui.GetWindowRect(gtawin)
-            top += 40  # for title bar
-            width = x2 - left + 1
-            height = y2 - top + 1
+    # get area by a window name
+    gtawin = win32gui.FindWindow(None, winName)
+    # get the bounding box of the window
+    left, top, x2, y2 = win32gui.GetWindowRect(gtawin)
+    top += 30  # for the title bar
+    width = x2 - left + 1
+    height = y2 - top + 1
 
-        hwindc = win32gui.GetWindowDC(desktop)
-        srcdc = win32ui.CreateDCFromHandle(hwindc)
-        memdc = srcdc.CreateCompatibleDC()
-        bmp = win32ui.CreateBitmap()
-        bmp.CreateCompatibleBitmap(srcdc, width, height)
-        memdc.SelectObject(bmp)
-        memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
+    # the device context(DC) for the entire window (title bar, menus, scroll bars, etc.)
+    hwindc = win32gui.GetWindowDC(desktop)
+    # Create a DC object from an integer handle
+    srcdc = win32ui.CreateDCFromHandle(hwindc)
+    # Create a memory device context that is compatible with the source DC
+    memdc = srcdc.CreateCompatibleDC()
+    # Create a bitmap object
+    bmp = win32ui.CreateBitmap()
+    # Create a bitmap compatible with the specified device context
+    bmp.CreateCompatibleBitmap(srcdc, width, height)
+    # Select an object into the device context.
+    memdc.SelectObject(bmp)
+    # Copy a bitmap from the source device context to this device context
+    # parameters: destPos, size, dc, srcPos, rop(the raster operation))
+    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
 
-        signedIntsArray = bmp.GetBitmapBits(True)
-        img = np.fromstring(signedIntsArray, dtype='uint8')
-        img.shape = (height, width, 4)
+    # the bitmap bits
+    signedIntsArray = bmp.GetBitmapBits(True)
+    # form a 1-D array initialized from text data in a string.
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (height, width, 4)
 
-        srcdc.DeleteDC()
-        memdc.DeleteDC()
-        win32gui.ReleaseDC(desktop, hwindc)
-        win32gui.DeleteObject(bmp.GetHandle())
+    # Delete all resources associated with the device context
+    srcdc.DeleteDC()
+    memdc.DeleteDC()
+    # Releases the device context
+    win32gui.ReleaseDC(desktop, hwindc)
+    # Delete the bitmap and freeing all system resources associated with the object.
+    # After the object is deleted, the specified handle is no longer valid.
+    win32gui.DeleteObject(bmp.GetHandle())
 
-        cv2.imshow('test', cv2.cvtColor(img, cv2.COLOR_BGRA2RGB))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
-
-    # return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
-
-
-if __name__ == "__main__":
-    grab_screen()
+    return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
