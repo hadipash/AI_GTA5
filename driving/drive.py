111471bfb333dc1ceb9ba9ddaf13f4fb2871ea41
Rustam <dknext99@gmail.com>
2018-04-21 14:31:58 +0900

    small adjustments to the driving module

--- driving/drive.py
+++ driving/drive.py
@@ -15,42 +15,57 @@ from keras.models import load_model
 # helper classes
 from data_collection.keycap import key_check
 from data_collection.screencap import grab_screen
-from playing.keycontrol import *
+# gamepad axes limits and gamepad module
+from driving.gamepad import AXIS_MIN, AXIS_MAX, TRIGGER_MAX, XInputDevice
 from training.utils import preprocess
 
-# init our model
+# init the model
 model = None
 path = "..\\training"
 
+# set scales for axes
+SCALE_AXIS_MIN = AXIS_MIN / -10
+SCALE_AXIS_MAX = AXIS_MAX / 10
+SCALE_TRIGGER = TRIGGER_MAX / 10
 
-def send_control(controls):
-    if controls == [1, 0]:
-        straight()
-    elif controls == [1, -1]:
-        forward_left()
-    elif controls == [1, 1]:
-        forward_right()
-    elif controls == [0, -1]:
-        left()
-    elif controls == [0, 1]:
-        right()
-    elif controls == [0, 0]:
-        no_keys()
-    elif controls == [-1, 0]:
-        reverse()
-    elif controls == [-1, -1]:
-        reverse_left()
+gamepad = None
+
+
+def set_gamepad(controls):
+    # trigger value
+    trigger = int(round(controls[0][0] * SCALE_TRIGGER))
+    if trigger >= 0:
+        # left trigger is zero
+        gamepad.SetTrigger('L', 0)
+        gamepad.SetTrigger('R', trigger)
     else:
-        reverse_right()
+        # inverse value
+        trigger = -trigger
+        # right trigger is zero
+        gamepad.SetTrigger('L', trigger)
+        gamepad.SetTrigger('R', 0)
+
+    # axis value
+    axis = 0
+    if controls[0][1] >= 0:
+        axis = int(round(controls[0][1] * SCALE_AXIS_MAX))
+    else:
+        axis = int(round(controls[0][1] * SCALE_AXIS_MIN))
+    gamepad.SetAxis('X', axis)
 
 
 def drive():
+    global gamepad
+    gamepad = XInputDevice(1)
+    gamepad.PlugIn()
+
     # countdown for having time to open GTA V window
     for i in list(range(5))[::-1]:
         print(i + 1)
         time.sleep(1)
     print("Start!")
 
+    # last_time = time.time()  # to measure the number of frames
     close = False  # to exit execution
     pause = False  # to pause execution
 
@@ -60,14 +75,23 @@ def drive():
             image = preprocess(cv2.resize(grab_screen("Grand Theft Auto V"), (320, 240)))
             image = np.array([image])  # the model expects 4D array
 
-            # predict the steering angle for the image
+            # predict the throttle and steering angle for the image
             controls = model.predict(image, batch_size=1)
-            controls = [round(float(controls[0][0])), round(float(controls[0][1]))]
-            send_control(controls)
+            # set the gamepad values
+            set_gamepad(controls)
+            # print("Throttle: {0:02d}, Steering: {0:02d}".format(int(round(controls[0][0])),
+            #       int(round(controls[0][1]))))
+
+            # print('Main loop took {} seconds'.format(time.time() - last_time))
+            # last_time = time.time()
 
             keys = key_check()
             if 'T' in keys:
                 pause = True
+                # release gamepad keys
+                gamepad.SetTrigger('L', 0)
+                gamepad.SetTrigger('R', 0)
+                gamepad.SetAxis('X', 0)
                 print('Paused. To exit the program press Z.')
                 time.sleep(0.5)
 
@@ -79,6 +103,7 @@ def drive():
         elif 'Z' in keys:
             close = True
             print('Closing the program.')
+            gamepad.UnPlug()
 
 
 def main():
