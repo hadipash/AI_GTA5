853b35d32cc1d75d464470883e567bad85c99f37
jia <legeny088@gmail.com>
2018-04-24 16:57:34 +0900

    object detection and labeling

---
+++ object_detection/detect.py
@@ -0,0 +1,37 @@
+# This code based on markjay4k code(https://github.com/markjay4k/YOLO-series/)
+
+import cv2
+from darkflow.net.build import TFNet
+import numpy as np
+import time
+
+option = {
+    'model': 'cfg/yolo.cfg',
+    'load': 'bin/yolov2.weights',
+    'threshold': 0.15
+}
+
+tfnet = TFNet(option)
+
+capture = cv2.VideoCapture('giphy.gif')
+colors = [tuple(255 * np.random.rand(3)) for i in range(5)]
+
+while (capture.isOpened()):
+    stime = time.time()
+    ret, frame = capture.read()
+    if ret:
+        results = tfnet.return_predict(frame)
+        for color, result in zip(colors, results):
+            tl = (result['topleft']['x'], result['topleft']['y'])
+            br = (result['bottomright']['x'], result['bottomright']['y'])
+            label = result['label']
+            frame = cv2.rectangle(frame, tl, br, color, 7)
+            frame = cv2.putText(frame, label, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+        cv2.imshow('frame', frame)
+        print('FPS {:.1f}'.format(1 / (time.time() - stime)))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        capture.release()
+        cv2.destroyAllWindows()
+        break
