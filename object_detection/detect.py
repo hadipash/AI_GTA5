205d0bbf841bd49da7ee6129c7309aa30cbe8426
jia-w <legeny088@gmail.com>
2018-04-26 01:49:13 +0900

    add object confidence

--- object_detection/detect.py
+++ object_detection/detect.py
@@ -8,7 +8,7 @@ import time
 option = {
     'model': 'cfg/yolo.cfg',
     'load': 'bin/yolov2.weights',
-    'threshold': 0.15
+    'threshold': 0.15,
 }
 
 tfnet = TFNet(option)
@@ -25,8 +25,11 @@ while (capture.isOpened()):
             tl = (result['topleft']['x'], result['topleft']['y'])
             br = (result['bottomright']['x'], result['bottomright']['y'])
             label = result['label']
+            confidence = result['confidence']
+            text = '{}: {:.0f}%'.format(label, confidence * 100)
             frame = cv2.rectangle(frame, tl, br, color, 7)
-            frame = cv2.putText(frame, label, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+            frame = cv2.putText(
+                frame, text, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2) 
         cv2.imshow('frame', frame)
         print('FPS {:.1f}'.format(1 / (time.time() - stime)))
         if cv2.waitKey(1) & 0xFF == ord('q'):
